7bf357583f9f331cbe9e1fe62e53cd69cc7bfb0b
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-08-10 19:00:28 -0400

    styled the to do taks added by the user

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const todosContainer = document.querySelector("#todosContainer");
 
 addTodoButton.addEventListener("click", () => {
     const newElement = document.createElement("div");
+    newElement.classList.add("todo")
     const value = addInput.value;
     newElement.innerHTML = `
-        <p>${value}</p>
-        <div>
+        <p class="todoText">${value}</p>
+        <div class="buttonsContainer">
             <span>Edit</span>
             <span>Delete</span>
         </div>
