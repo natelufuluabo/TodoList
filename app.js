421fa9788d615ba663c58e89d7f3ef090b272cd7
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-08-10 20:08:53 -0400

    implemented the ability to edit todo

--- app.js
+++ app.js
@@ -9,12 +9,34 @@ addTodoButton.addEventListener("click", () => {
     newElement.classList.add("todo")
     const value = addInput.value;
     newElement.innerHTML = `
-        <p class="todoText">${value}</p>
-        <div class="buttonsContainer">
-            <span>Edit</span>
-            <span>Delete</span>
+        <div id="inputContainer" class="inputContainer hidden">
+            <input id="editInput" class="addInput" type="text" placeholder="Enter to do here" />
+            <button id="updateTodoButton" class="addTodoButton">Update</button>
+        </div>
+        <p id="todoText" class="todoText">${value}</p>
+        <div id="buttonsContainer" class="buttonsContainer">
+            <span id="editButton">Edit</span>
+            <span id="deleteButton">Delete</span>
         </div>
     `;
     todosContainer.append(newElement);
+    document.querySelector("#editButton").addEventListener("click", () => {
+        document.querySelector("#editInput").value = document.querySelector("#todoText").innerHTML;
+        document.querySelector(".todo").classList.add("todo2");
+        document.querySelector(".todo2").classList.remove("todo");
+        document.querySelector("#todoText").classList.add("hidden");
+        document.querySelector("#buttonsContainer").classList.add("hidden");
+        document.querySelector("#inputContainer").classList.remove("hidden");
+    });
+
+    document.querySelector("#updateTodoButton").addEventListener("click", () => {
+        document.querySelector("#todoText").innerHTML = document.querySelector("#editInput").value;
+        document.querySelector(".todo2").classList.add("todo");
+        document.querySelector(".todo").classList.remove("todo2");
+        document.querySelector("#todoText").classList.remove("hidden");
+        document.querySelector("#buttonsContainer").classList.remove("hidden");
+        document.querySelector("#inputContainer").classList.add("hidden");
+    });
     addInput.value = "";
 });
+
